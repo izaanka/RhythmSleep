e88b74a271a1e33324cc923c40ff9856c363de0f
Izaan Kaskar <izaankaskar7@gmail.com>
2026-06-21 18:31:24 +0530

    Update EEG_Sleep.ino

--- EEG_Sleep/EEG_Sleep.ino
+++ EEG_Sleep/EEG_Sleep.ino
@@ -5,24 +5,21 @@
 #define EEG_PIN A0
 
 arduinoFFT FFT = arduinoFFT();
-
 unsigned int sampling_period_us;
 unsigned long microseconds;
 
 double vReal[SAMPLES];
 double vImag[SAMPLES];
 
-// Flag to control the processing loop
 bool alarmTriggered = false;
 
 void setup() {
   Serial.begin(115200);
   sampling_period_us = round(1000000.0 / SAMPLING_FREQUENCY);
-  analogReadResolution(12); 
+  analogReadResolution(12);
 }
 
 void loop() {
-  // Halt all execution if the Linux MPU sent the wake command
   if (alarmTriggered) {
     delay(10000);
     return; 
@@ -30,7 +27,6 @@ void loop() {
 
   for (int i = 0; i < SAMPLES; i++) {
     microseconds = micros();
-    
     vReal[i] = analogRead(EEG_PIN);
     vImag[i] = 0; 
     
@@ -42,10 +38,8 @@ void loop() {
   FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
 
   double delta = 0, theta = 0, alpha = 0, beta = 0;
-  
   for (int i = 1; i < (SAMPLES / 2); i++) {
     double freq = (i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES;
-    
     if (freq >= 0.5 && freq <= 4.0) delta += vReal[i];
     else if (freq > 4.0 && freq <= 8.0) theta += vReal[i];
     else if (freq > 8.0 && freq <= 13.0) alpha += vReal[i];
@@ -53,21 +47,22 @@ void loop() {
   }
 
   Serial.print("State: ");
+  
+  // Updated string payload logic to match the frontend array
   if (delta > theta && delta > alpha && delta > beta) {
-    Serial.println("Deep Sleep");
+    Serial.println("Deep");
   } else if (theta > delta && theta > alpha && theta > beta) {
-    Serial.println("Light Sleep / REM");
+    Serial.println("Core"); 
   } else if (alpha > theta && alpha > beta) {
-    Serial.println("Relaxed Awake");
+    Serial.println("REM");  
   } else {
-    Serial.println("Active / Focused");
+    Serial.println("Awake"); 
   }
 
-  // Check for incoming commands from the Python script
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
     if (command.indexOf("WAKE") >= 0) {
-      alarmTriggered = true; // Stop processing
+      alarmTriggered = true;
     }
   }
 
